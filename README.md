c6d2c50e9c1e2b5008008267d3854eb8543dee35
resistdesign <ryantgraff@resistdesign.com>
2020-06-05 22:55:09 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-m3jokv
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-m3jokv)
---
+++ index.tsx
@@ -0,0 +1,97 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+import Hello from "./Hello";
+import "./style.css";
+import {
+  IncarnateRouter,
+  IncarnateRoute,
+  IncarnateSwitch
+} from "incarnate-dom";
+
+interface AppProps {}
+interface AppState {
+  firstName: string;
+  lastName: string;
+}
+
+class App extends Component<AppProps, AppState> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: "Ryan",
+      lastName: "Graff"
+    };
+  }
+
+  onNameInputChange = (event: {
+    nativeEvent: {
+      data: string;
+    };
+    target: {
+      value: string;
+      selectionStart: number;
+      selectionEnd: number;
+      setSelectionRange: Function;
+    };
+    preventDefault: Function;
+  }) => {
+    const {
+      nativeEvent: { data = "" } = {},
+      target,
+      target: { value: name = "", selectionStart = 0, selectionEnd = 0 } = {}
+    } = event;
+    const [firstName = "", lastName = ""] = name.split("⏎");
+
+    if (data === "⏎" || name.indexOf("⏎") === -1) {
+      event.preventDefault();
+      target.setSelectionRange(selectionStart, selectionEnd);
+
+      return;
+    }
+
+    this.setState({ firstName, lastName });
+  };
+
+  onNameInputKeyDown = ({ target, target: {value = ''}, nativeEvent: { keyCode, shiftKey } }) => {
+    const { selectionStart = 0, selectionEnd = 0 } = target;
+    
+    if(keyCode === 8 && value[selectionStart - 1] === "⏎"){
+      event.preventDefault();
+    }else if (keyCode === 13 && shiftKey) {
+      event.preventDefault();
+      target.setRangeText("⏎");
+      target.setSelectionRange(selectionStart + 1, selectionEnd + 1);
+    }
+  };
+
+  render() {
+    const { firstName = "", lastName = "" } = this.state;
+
+    return (
+      <IncarnateRouter name="Thing">
+        <div>
+          <a href="home">Home</a>
+          <a href="about">About</a>
+        </div>
+        <IncarnateRoute subPath="/">
+          <IncarnateSwitch defaultSubPath="home">
+            <IncarnateRoute subPath="home">
+              <h1>Home</h1>
+            </IncarnateRoute>
+            <IncarnateRoute subPath="about">
+              <pre>{JSON.stringify(this.state, null, "  ")}</pre>
+              <textarea
+                placeholder="Name"
+                value={[firstName, lastName].join("⏎")}
+                onChange={this.onNameInputChange}
+                onKeyDown={this.onNameInputKeyDown}
+              />
+            </IncarnateRoute>
+          </IncarnateSwitch>
+        </IncarnateRoute>
+      </IncarnateRouter>
+    );
+  }
+}
+
+render(<App />, document.getElementById("root"));
