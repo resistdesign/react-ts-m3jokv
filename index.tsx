995cfd1556683021fc11a555ece48b48666e8aba
resistdesign <ryantgraff@resistdesign.com>
2020-06-05 23:02:35 -0400

    1 file modified

--- index.tsx
+++ index.tsx
@@ -73,6 +73,7 @@ class App extends Component<AppProps, AppState> {
             </IncarnateRoute>
             <IncarnateRoute subPath="about">
               <pre>{JSON.stringify(this.state, null, "  ")}</pre>
+              <small>(Press SHIFT+ENTER to Add a new field)</small>
               <textarea
                 placeholder="Name"
                 value={[firstName, lastName].join("⏎")}
